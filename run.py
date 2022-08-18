564595ed83c091770dad3e10570884214d8ce378
Nigel Doheny <nigel.doheny93@gmail.com>
2022-08-18 15:54:54 +0000

    Add Index and about html to open in flask

--- run.py
+++ run.py
@@ -1,18 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
 @app.route("/")
-
 def index():
-        return "Hello world"
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
-    app.run(
-        host=os.environ.get("IP","0.0.0.0"),
+    app. run(
+        host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True
     )
